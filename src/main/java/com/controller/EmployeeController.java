02aba42fca6185335a976d454ffd70686bbae699
bhavya-dev <77583740+bhavya-dev@users.noreply.github.com>
2021-03-16 19:47:49 +0530

    Added checkbox

--- src/main/java/com/controller/EmployeeController.java
+++ src/main/java/com/controller/EmployeeController.java
@@ -7,10 +7,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.model.Employee;
@@ -62,11 +64,6 @@ public class EmployeeController {
 		return "redirect:/";
 	}
 	
-	@RequestMapping("/checkMethod")
-	public String checkMethod(@ModelAttribute("employee") Employee employee) {
-    employeeService.saveEmployee(employee);
-    System.out.println(employee);
-		return "redirect:/";
-	}
+	
 
 }
